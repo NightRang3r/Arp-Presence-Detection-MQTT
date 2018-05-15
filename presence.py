8c3603c0613d582061083cf2766a2da83d604042
Shai rod <shai@exploit.co.il>
2018-05-15 11:05:36 +0300

    Added Error Handling

--- presence.py
+++ presence.py
@@ -9,32 +9,27 @@ MQTT_HOST = "127.0.0.1"
 MQQT_USER = "mosquitto"
 MQTT_PASS = "12345678"
 MQTT_TOPIC = "presence"
-T_SLEEP = 30
-
+T_SLEEP = 10
 INTERFACE = "eth0"
-NAMES = ["Bob","Alice"]
-MAC_ADDR = ["01:01:01:01:01:01","02:02:02:02:02:02"]
+DEVICES= {"Bob": "00:00:00:00:00:00", "Alice":"01:01:01:01:01:01"}
 
 #========================= END SETTINGS =========================
 
 auth = {
-   'username':MQQT_USER,
-   'password':MQTT_PASS 
+	'username':MQQT_USER,
+	'password':MQTT_PASS 
 }
-       
-try:
-    while True:
-        output = subprocess.check_output("sudo arp-scan --interface=" + INTERFACE + " --localnet", shell=True)
-        for i in range(len(NAMES)):
-            if MAC_ADDR[i] in output:
-                print(NAMES[i] + " is home")
-                publish.single(MQTT_TOPIC + "/" + NAMES[i],"home",hostname=MQTT_HOST, auth=auth)         
-            else:
-                print(NAMES[i] + " is not_home")
-                publish.single(MQTT_TOPIC + "/" + NAMES[i],"not_home",hostname=MQTT_HOST, auth=auth)
-        sleep(T_SLEEP)
 
-except:
-    print "Something went wrong..."
-    
-
+while True:
+	try:
+		output = subprocess.check_output("sudo arp-scan --interface=" + INTERFACE + " --localnet", shell=True)
+		for device in DEVICES:
+			if DEVICES[device].lower() in output:
+				print(device + " is home")
+				publish.single(MQTT_TOPIC + "/" + device,"home",hostname=MQTT_HOST, auth=auth)         
+			else:
+				print(device + " is not_home")
+				publish.single(MQTT_TOPIC + "/" + device,"not_home",hostname=MQTT_HOST, auth=auth)
+	except:
+			print "Something went wrong!"
+	sleep(T_SLEEP)
